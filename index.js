1bcf416a0399b0e129215fccdc9a80bbea118cc2
jadson-siqueira <jadsomoficial2023@gmail.com>
2024-06-05 20:56:50 -0300

    atualização

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("ola mundo")
+let heroi ="jadson";
+
+let xp = 0;
+
+
+if (xp >=0 <= 1000){
+    console.log("o heroi de nome " + heroi + " atingiu a patente Ferro")
+};
+
+
+if (xp >= 1001 <= [2000]){
+    console.log("o heroi de nome " + heroi + " atingiu a patente Bronze")
+};
+
+if (xp >= 2001 <= 5000){
+    console.log("o heroi de nome " + heroi + " atingiu a patente Prata")
+};
+
+if (xp >= 5001 <= 7000){
+    console.log("o heroi de nome " + heroi + " atingiu a patente Ouro")
+};
+
+if (xp >= 7001 <= 8000){
+    console.log("o heroi de nome " + heroi + " atingiu a patente Platina")
+}
+;
+if (xp >= 8001 <= 9000){
+    console.log("o heroi de nome " + heroi + " atingiu a patente Ascendente")
+};
+
+if (xp >= 9001 <= 10000){
+    console.log("o heroi de nome " + heroi + " atingiu a patente Imortal")
+};
+
+if (xp >= 10001){
+    console.log("o heroi de nome " + heroi + " atingiu a patente Radiante")
+};
