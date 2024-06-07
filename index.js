4f5c739a6e3d87e6ba5ac949157bd85906892ebc
jadson-siqueira <jadsomoficial2023@gmail.com>
2024-06-07 20:06:43 -0300

    desafioFelipão

--- index.js
+++ index.js
@@ -1,37 +1,32 @@
-let heroi ="jadson";
 
-let xp = 0;
+ let heroi = "jadson"
 
+ let xp = 900100000000
 
-if (xp >=0 <= 1000){
-    console.log("o heroi de nome " + heroi + " atingiu a patente Ferro")
-};
-
-
-if (xp >= 1001 <= [2000]){
-    console.log("o heroi de nome " + heroi + " atingiu a patente Bronze")
-};
-
-if (xp >= 2001 <= 5000){
-    console.log("o heroi de nome " + heroi + " atingiu a patente Prata")
-};
-
-if (xp >= 5001 <= 7000){
-    console.log("o heroi de nome " + heroi + " atingiu a patente Ouro")
-};
-
-if (xp >= 7001 <= 8000){
-    console.log("o heroi de nome " + heroi + " atingiu a patente Platina")
-}
-;
-if (xp >= 8001 <= 9000){
-    console.log("o heroi de nome " + heroi + " atingiu a patente Ascendente")
-};
-
-if (xp >= 9001 <= 10000){
-    console.log("o heroi de nome " + heroi + " atingiu a patente Imortal")
-};
-
-if (xp >= 10001){
-    console.log("o heroi de nome " + heroi + " atingiu a patente Radiante")
-};
+ if (xp <= 1000){
+   console.log("o heroi de nome " + heroi + " atingiu a patente Ferro")
+ }
+ else if (xp >= 1001 && xp <= 2000){
+   console.log("o heroi de nome " + heroi + " atingiu a patente Bronze")
+ }
+ else if (xp >= 2001 && xp <= 5000){
+   console.log("o heroi de nome " + heroi + " atingiu a patente Prata")
+ }
+ else if(xp >= 5001 && xp <= 7000){
+   console.log("o heroi de nome " + heroi + " atingiu a patente Ouro")
+ }
+ else if (xp >= 7001 && xp <= 8000){
+   console.log("o heroi de nome " + heroi + " atingiu a patente Platina")
+ }
+ else if (xp >= 8001 && xp <= 9000){
+   console.log("o heroi de nome " + heroi + " atingiu a patente Ascendente")
+ }
+ else if (xp >= 9001 && xp <= 10000){
+   console.log("o heroi de nome " + heroi + " atingiu a patente Imortal")
+ }
+ else if (xp >= 10001 && xp <= 20000){
+   console.log("o heroi de nome " + heroi + " atingiu a patente Radiante")
+ }
+ else {
+   console.log ("limite atingido")
+ }
